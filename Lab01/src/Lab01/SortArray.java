d180a101defb6565227830afd08d3fcb8783acd9
Selina-th4 <103742905+Selina-th4@users.noreply.github.com>
2023-04-27 11:32:27 +0700

    Create SortArray.java

---
+++ Lab01/src/Lab01/SortArray.java
@@ -0,0 +1,47 @@
+package Lab01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortArray {
+
+    public static void main(String[] args) {
+        
+        // initialize scanner for user input
+        Scanner input = new Scanner(System.in);
+        
+        // ask user to enter array length
+        System.out.print("Enter array length: ");
+        int length = input.nextInt();
+        
+        // initialize array
+        int[] array = new int[length];
+        
+        // ask user to enter array elements
+        System.out.print("Enter array elements: ");
+        for (int i = 0; i < length; i++) {
+            array[i] = input.nextInt();
+        }
+        
+        // sort array in ascending order
+        Arrays.sort(array);
+        
+        // calculate sum of array elements
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += array[i];
+        }
+        
+        // calculate average of array elements
+        double average = (double) sum / length;
+        
+        // print sorted array, sum, and average
+        System.out.println("Sorted array: " + Arrays.toString(array));
+        System.out.println("Sum of array elements: " + sum);
+        System.out.println("Average of array elements: " + average);
+        
+        // close scanner
+        input.close();
+    }
+
+}
