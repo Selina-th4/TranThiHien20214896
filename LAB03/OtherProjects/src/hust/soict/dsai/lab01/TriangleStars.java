309bdd49b10450d4107ddb3981c780ae95d3281f
Selina-th4 <103742905+Selina-th4@users.noreply.github.com>
2023-05-15 19:23:06 +0700

    Create TriangleStars.java

---
+++ LAB03/OtherProjects/src/hust/soict/dsai/lab01/TriangleStars.java
@@ -0,0 +1,20 @@
+package hust.soict.dsai.lab01;
+
+import java.util.Scanner;
+
+public class TriangleStars {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the height of the triangle: ");
+        int height = scanner.nextInt();
+        scanner.close();
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
